6de135be74a119b4d91f6d01fcbddce98f70f42f
BrayanRojasLemus <brayanrojas@wposs.com>
2023-06-21 08:25:22 -0500

    Agregar archivos de proyecto.

---
+++ CrudCore/Datos/Conexion.cs
@@ -0,0 +1,18 @@
+﻿//Realizamoa la conexion con ADO.NET    
+
+using System.Data.SqlClient;
+
+namespace CrudCore.Datos
+{
+    public class Conexion
+    {
+        private string cadenaSQL = string.Empty;
+
+        public Conexion()
+        {
+            //Obtenemos la cadena de conexion que esta en el archivo appsettings.json
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();  
+        }
+
+    }
+}
